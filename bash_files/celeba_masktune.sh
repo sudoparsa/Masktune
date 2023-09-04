c6c06b638dc6a24103b77bc5c544ed6302cb49ab
Parsa Hosseini <sp.hoseiny@gmail.com>
2023-09-04 17:17:48 +0900

    Update celeba_masktune.sh

--- bash_files/celeba_masktune.sh
+++ bash_files/celeba_masktune.sh
@@ -3,17 +3,17 @@ cd /home/user01/masktune/Masktune
 
 python3 -m src.main --dataset celeba \
                     --arch resnet50 \
-                    --base_dir /home/user01/masktune/Masktune \
+                    --base_dir /content/Masktune \
                     --lr 0.0001 \
                     --use_cuda \
                     --optimizer sgd \
-                    --train_batch 512 \
-                    --test_batch 512 \
-                    --masking_batch 256 \
+                    --train_batch 256 \
+                    --test_batch 256 \
+                    --masking_batch 128 \
                     --epochs 20 \
                     --gamma 1.0 \
                     --weight_decay 0.0001 \
                     --masktune \
                     --use_pretrained_weights \
-                    --dataset_dir /home/user01/hosseini/celebA \
-                    --masktune_iterations 3
+                    --dataset_dir /content/drive/MyDrive/Meta_ood/data/celeba/celeba \
+                    --masktune_iterations 1
